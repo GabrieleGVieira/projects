a33f9909cd3f4d79d24ec0e628469c848909ac2a
Gabriele Gonçalves Vieira <gabrielevieira011@gmail.com>
2024-06-03 10:10:14 -0300

    feat: config github pages

---
+++ node_modules/.cache/gh-pages/https!github.com!GabrieleGVieira!projects.git/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+function Footer() {
+  return (
+    <Box bg="white" p={4} textAlign="center">
+      <Text>© Developed with great heart and big dreams.</Text>
+    </Box>
+  );
+}
+
+export default Footer;
